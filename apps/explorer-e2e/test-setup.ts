16ea53899faba90f48beabeb59be336291d391b0
William Mendonça <william@tecmasternet.com.br>
2023-06-05 15:13:26 -0300

    feat: :zap: e2e test

--- apps/explorer-e2e/test-setup.ts
+++ apps/explorer-e2e/test-setup.ts
@@ -3,3 +3,21 @@ import { device } from 'detox';
 beforeAll(async () => {
   await device.launchApp();
 });
+
+afterAll(async () => {
+  await device.terminateApp();
+}
+
+// Path: apps/explorer-e2e/src/app.e2e-spec.ts
+import { expect, element, by } from 'detox';
+
+describe('Example', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should have welcome screen', async () => {
+    await expect(element(by.id('welcome'))).toBeVisible();
+  });
+}
+
